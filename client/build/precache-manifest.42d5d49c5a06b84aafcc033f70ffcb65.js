5f99d30609ef93b304cc3197907e4b773ed34211
Suguna Devi <sugunadevi14@gmail.com>
2019-08-21 03:12:10 +0530

    Add files via upload

---
+++ client/build/precache-manifest.42d5d49c5a06b84aafcc033f70ffcb65.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11421ffa26f9f2a8e1944216cee53263",
+    "url": "/index.html"
+  },
+  {
+    "revision": "98065c123def2a3c1903",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "0e7973adc9c54792c5ac",
+    "url": "/static/css/main.4eb22f0b.chunk.css"
+  },
+  {
+    "revision": "98065c123def2a3c1903",
+    "url": "/static/js/2.b0a3ea9a.chunk.js"
+  },
+  {
+    "revision": "0e7973adc9c54792c5ac",
+    "url": "/static/js/main.d2d7744c.chunk.js"
+  },
+  {
+    "revision": "1a200386bc84e6f5bd99",
+    "url": "/static/js/runtime~main.0c12c440.js"
+  }
+]);
